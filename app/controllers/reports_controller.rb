3773fa5a980e46333eb79b196de678f1d978d6b0
Dwight Scott -- Kubuntu <dscotts3@gmail.com>
2020-07-25 18:31:14 -0400

    ensure search was searching between beginnging of day and end of day

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,12 +5,15 @@ class ReportsController < ApplicationController
   end
 
   def sales
-    @products = Product.with_sales.group(:id).paginate(page: params[:page])
     @order_items = OrderItem.sold
+    @products = Product.with_sales
+
     unless dates.empty?
-      @products = @products.between([dates[:start_date], dates[:end_date]])
-      @order_items = @order_items.between([dates[:start_date], dates[:end_date]])
+      @products = @products.between([dates[:start], dates[:end]])
+      @order_items = @order_items.between([dates[:start], dates[:end]])
     end
+
+    @products = @products.group(:id).paginate(page: params[:page])
   end
 
   def coupons
@@ -21,11 +24,11 @@ class ReportsController < ApplicationController
 
   def dates
     return {} if params[:start].blank? || params[:end].blank?
-    start_date = %w(1 2 3).map { |e| params["start"]["(#{e}i)"].to_i }
-    end_date = %w(1 2 3).map { |e| params["end"]["(#{e}i)"].to_i }
+    starting = %w(1 2 3).map { |d| params["start"]["(#{d}i)"].to_i }
+    ending = %w(1 2 3).map { |d| params["end"]["(#{d}i)"].to_i }
     {
-      start_date: Date.new(start_date[0], start_date[1], start_date[2]),
-      end_date: Date.new(end_date[0], end_date[1], end_date[2])
+      start: Date.new(starting[0], starting[1], starting[2]).beginning_of_day,
+      end: Date.new(ending[0], ending[1], ending[2]).end_of_day
     }
   end
 
